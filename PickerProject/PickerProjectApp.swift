18a30d9b3a574874355bcdb38ffbc7596b0360e6
Anna <anna.vasi4ko@icloud.com>
2022-09-08 14:59:58 +0300

    Initial Commit

---
+++ PickerProject/PickerProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickerProjectApp.swift
+//  PickerProject
+//
+//  Created by Vasichko Anna on 08.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct PickerProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
