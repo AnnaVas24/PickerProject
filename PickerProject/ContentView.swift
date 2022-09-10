cb1023222703731cf37bce70e50b20ca8ac32c45
Anna <anna.vasi4ko@icloud.com>
2022-09-10 12:37:47 +0300

    picket project

--- PickerProject/ContentView.swift
+++ PickerProject/ContentView.swift
@@ -7,15 +7,63 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+enum GoodsCategory: String, CaseIterable, Identifiable {
+    case clothes
+    case shoes
+    case food
+    case electronics
+    
+    var id: Self {
+        self
+    }
+    
+    var title: String {
+        switch self {
+        case .clothes:
+            return "Clothes"
+        case .shoes:
+            return "Shoes"
+        case .food:
+            return "Food"
+        case .electronics:
+            return "Electronics"
         }
-        .padding()
+    }
+}
+
+struct ContentView: View {
+    
+    @State private var selectedItem: GoodsCategory = .clothes
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                Picker("Goods", selection: $selectedItem) {
+                    ForEach(GoodsCategory.allCases) { category in
+                        HStack {
+                            Text(category.title).tag(category)
+                            Spacer()
+                            switch category {
+                            case .clothes:
+                                Image(systemName: "tshirt.fill")
+                            case .shoes:
+                                Image(systemName: "magnifyingglass.circle")
+                            case .food:
+                                Image(systemName: "fork.knife")
+                            case .electronics:
+                                Image(systemName: "headphones")
+                            }
+                        }
+                    }
+                    
+                }
+                HStack {
+                    Text("You search in:")
+                    Text(selectedItem.rawValue)
+                }
+            }
+            .navigationTitle("Store")
+        }
     }
 }
 
